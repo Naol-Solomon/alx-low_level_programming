c7b5285536f27e32d6bd202117406f91c5dcdf99
Naol Solomon <naolsolomon10@gmail.com>
2023-07-25 11:47:07 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
- * print_rev - print string in reverse followed by newline
- * @s: parameter it takes
- * Return: Always 0
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
 void print_rev(char *s)
 {
-	int count = 0;
-	int r;
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
-		count++;
+		longi++;
 		s++;
 	}
 	s--;
-	for (r =count ; r > 0; r--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
