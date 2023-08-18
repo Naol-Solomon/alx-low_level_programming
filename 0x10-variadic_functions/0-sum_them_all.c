d112902919f18ac87964d013babb670dfa7daef4
Naol Solomon <naolsolomon10@gmail.com>
2023-08-18 15:33:45 -0700

    task updated

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all- operation on variadic function
+ *
+ *@n: number of parameters the function take
+ *@...: variability of number of parameters
+ *
+ *Return: if n == 0-0 
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+
+	if (list == 0)
+	{
+		return (0);
+	}
+	unsigned int i, sum = 0;
+
+	va_start(list, n);
+	for (i = 0; i < n ; i++)
+	{
+		sum += va_arg(list, const unsigned int);
+	}
+
+	va_end(list);
+	return (sum);
+}
