c8b5fd515ba502f980cb87f712d0f70a9192680a
Naol Solomon <naolsolomon10@gmail.com>
2023-08-18 15:43:59 -0700

    task updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
+	unsigned int i, sum = 0;
 
 	if (list == 0)
 	{
 		return (0);
 	}
-	unsigned int i, sum = 0;
-
 	va_start(list, n);
 	for (i = 0; i < n ; i++)
 	{
