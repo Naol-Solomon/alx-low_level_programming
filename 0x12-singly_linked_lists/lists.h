16cdd77d869f327468c9529d7f427b784c22d953
Naol-Solomon <naolsolomon10@gmail.com>
2023-08-24 19:58:57 -0700

    task updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,22 @@
-#ifndef list
-#define list
-size_t print_list(const list_t *h)
+#ifndef LIST_H
+#define lIST_H
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
+
+size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 #endif
