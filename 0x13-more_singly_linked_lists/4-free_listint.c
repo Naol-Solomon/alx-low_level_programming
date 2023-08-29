b19758d0c800fb2a7a4846880f909effc1f167a0
Naol-Solomon <naolsolomon10@gmail.com>
2023-08-29 14:31:26 -0700

    task 4 done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - free location taken
+ * @head: pointer to function
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
