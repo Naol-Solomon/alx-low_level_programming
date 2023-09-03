f5e019cce58ab52ab576be1871fd702240a712d7
Naol-Solomon <naolsolomon10@gmail.com>
2023-09-03 15:41:31 -0700

    task 6 completed

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * get_endianess- gets endians
+ * get_endianness- gets endians
  * Return: returns 1 or 0
  */
-int get_endianess(void)
+int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
