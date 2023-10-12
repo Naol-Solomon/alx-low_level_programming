82b61bf40b085c2eac67b0d83c75930389f74100
Naol Solomon <naolsolomon10@gmail.com>
2023-10-12 14:39:09 +0300

    task zero completed

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_dlistint - print a list of integers in the given lists
+ * @h: head pointer to the head of the list
+ * Return: number of elements in the list
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t number = 0;
+
+	if (h == NULL)
+	{
+		printf("no data!");
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		const dlistint_t *temp = h;
+
+		while (temp != NULL)
+		{
+			number++;
+			printf("%d\n", temp->n);
+			temp = temp->next;
+		}
+	}
+	return (number);
+}
